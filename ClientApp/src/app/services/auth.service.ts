71314641e91daaf7e64e0215e3c264b266158630
Michal Delura <deluramichal@gmail.com>
2018-09-29 19:01:30 +0200

    Get user's profile

--- ClientApp/src/app/services/auth.service.ts
+++ ClientApp/src/app/services/auth.service.ts
@@ -7,30 +7,32 @@ import * as auth0 from 'auth0-js';
 
 @Injectable()
 export class AuthService {
-
+  profile: any;
   auth0 = new auth0.WebAuth({
     clientID: '13dZ4PzaxyJRllG1LrVWsbV5nDx67Xsi',
     domain: 'vegaprojectmd.eu.auth0.com',
     responseType: 'token id_token',
     redirectUri: 'https://localhost:5001',
-    scope: 'openid'
+    scope: 'openid email'
   });
 
-  constructor(public router: Router) {}
+  constructor(public router: Router) {
+    this.profile = JSON.parse(localStorage.getItem('profile'));
+  }
 
   public login(): void {
     this.auth0.authorize();
   }
 
   public handleAuthentication(): void {
-    this.auth0.parseHash((err, authResult) => {
+    this.auth0.parseHash((error, authResult) => {
       if (authResult && authResult.accessToken && authResult.idToken) {
         window.location.hash = '';
         this.setSession(authResult);
         this.router.navigate(['']);
-      } else if (err) {
+      } else if (error) {
         this.router.navigate(['']);
-        console.log(err);
+        throw error;
       }
     });
   }
@@ -41,6 +43,14 @@ export class AuthService {
     localStorage.setItem('access_token', authResult.accessToken);
     localStorage.setItem('id_token', authResult.idToken);
     localStorage.setItem('expires_at', expiresAt);
+
+    this.auth0.client.userInfo(authResult.accessToken, (error, profile) => {
+      if (error)
+        throw error;
+      localStorage.setItem('profile', JSON.stringify(profile));
+      this.profile = profile;
+      });
+
   }
 
   public logout(): void {
@@ -48,6 +58,8 @@ export class AuthService {
     localStorage.removeItem('access_token');
     localStorage.removeItem('id_token');
     localStorage.removeItem('expires_at');
+    localStorage.removeItem('profile');
+    this.profile = null;
     // Go back to the home route
     this.router.navigate(['/']);
   }
